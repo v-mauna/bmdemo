1df5bd12d5ace4c2b7616f4ed40f8526966a4ec4
Vanessa Mauna <vmauna@aol.com>
2020-07-24 10:55:44 -0500

    Modified Navbar and Media Queries

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function displayVideo() {
 	const contentText = document.getElementById('content-text');
 	const videoDiv = document.getElementById('my-video');
 	const credits = document.getElementById('credits');
-	credits.style.display = 'none';
 	contentText.style.display = 'none';
 	videoDiv.currentTime = 0;
 	videoDiv.pause();
